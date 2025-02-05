5760cffe0d2ee1d58c4f7921619fde9927394610
Victoria-vee <vickyessien1@gmail.com>
2025-02-05 12:46:16 +0100

    Added a reset button function and made thhe start and stop button work properly

--- pomodoro.js
+++ pomodoro.js
@@ -4,7 +4,7 @@ let seconds = 60;
 let isrunning = false;
 let countdown;
 
-
+// Start and stop timer function
 function myfunction() {
     if (isrunning) {
         clearInterval(countdown);
@@ -12,16 +12,38 @@ function myfunction() {
     }
     else {
         countdown = setInterval(() => {
+            if (seconds ===0 && minutes === 0){
+            clearInterval(countdown);
+            minutes= 24;
+            seconds = 60;
+            time.innerText = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            document.getElementById("start-timer").innerText = "Start";
+            isrunning = false;
+            }
+
             seconds--;
-            if (seconds == 0) {
+            if (seconds < 0) {
                 seconds = 59;
                 minutes--;
             }
-            time.innerText = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+            time.innerText = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
         }
         , 1000)
         document.getElementById("start-timer").innerText = "Stop";
     }
     isrunning = !isrunning;
+
+    
+    
 }
 
+// Reset timer function
+function resetbutton() {
+        clearInterval(countdown);
+        minutes = 24;
+        seconds = 60;
+        time.innerText = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+        document.getElementById("start-timer").innerText = "Start";
+        isrunning = false;
+    }

9a233faa7c797995bee7f57a8eec007c4aa22596
Victoria-vee <vickyessien1@gmail.com>
2025-02-11 16:16:04 +0100

    `Added new features to Pomodoro timer: task list with add and delete functionality, horizontal line styling, and task list persistence using local storage.`

--- todo.js
+++ todo.js
@@ -6,11 +6,36 @@ function addtask(){
         alert("No Task");
     }
     else{
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        list.appendChild(li);
+        let task = document.createElement("li");
+        task.innerHTML = input.value;
+        list.appendChild(task);
+        let trash = document.createElement("i");
+        trash.className = "fa-solid fa-trash"
+        task.appendChild(trash);
     
     }
     
     input.value = "";
+    saveData();
 }
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "I"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("taskData", list.innerHTML);
+}
+
+function displayData(){
+    list.innerHTML = localStorage.getItem("taskData");
+}
+
+displayData();
